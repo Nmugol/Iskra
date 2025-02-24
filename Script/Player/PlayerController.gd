37052dbe08b9398d18a40a7b459aeb94e0f74010
Nmugol <44527408+Nmugol@users.noreply.github.com>
2025-02-24 19:25:53 +0100

    Dodanie modeli kamieni

--- Script/Player/PlayerController.gd
+++ Script/Player/PlayerController.gd
@@ -1,35 +1,56 @@
 extends CharacterBody2D
 
 @export var speed: float = 300
+@export var acceleration: float = 10
+@export var stop_threshold: float = 5
+
+@export var Navigation: NavigationAgent2D
+
+var Region: NavigationRegion2D
+var map
 
 var mouse_pos = Vector2()
-var target_pos = Vector2()
 
-func _ready()->void:
-    Signals.connect("save_game" , SavePlayerData)
-    
+func _ready() -> void:
+    Region = get_parent().get_node("%Region")
+    if Region:
+        map = Region.get_navigation_map()
+    else:
+        push_error("NavigationRegion2D not found!")
+
+    Signals.connect("save_game", SavePlayerData)
     SetUp()
+
+func IsPointInRegion(point: Vector2) -> bool:
+    if map == null:
+        return false  # Zabezpieczenie przed brakiem mapy nawigacyjnej
+    
+    var closest_point = NavigationServer2D.map_get_closest_point(map, point)
+    return closest_point == point
 
 func _physics_process(delta: float) -> void:
     if Input.is_action_just_pressed("MovePlayer"):
-        mouse_pos = get_global_mouse_position()
-        print("mouse pos: ", mouse_pos)
+        var temp = get_global_mouse_position()
+        if IsPointInRegion(temp):
+            mouse_pos = temp
+            Save.PlayerPosition = mouse_pos
+            Navigation.target_position = mouse_pos
 
-        Save.PlayerPosition = mouse_pos
-
-    if position.distance_to(mouse_pos) > 3:
-        target_pos = (mouse_pos-position).normalized()
-        velocity = target_pos * speed
-        move_and_slide()
+    if position.distance_to(mouse_pos) > stop_threshold:
+        var direction = (Navigation.get_next_path_position() - global_position).normalized()
+        velocity = velocity.lerp(direction * speed, acceleration * delta)
 
         Signals.emit_signal("save_game")
         Signals.emit_signal("save_to_file")
-    pass
+    else:
+        velocity = Vector2.ZERO  # Zatrzymanie postaci
+    
+    move_and_slide()
 
-func SavePlayerData()->void:
+func SavePlayerData() -> void:
     Save.PlayerPosition = position
     Save.Equipment = []
 
-func SetUp()->void:
+func SetUp() -> void:
     position = Save.PlayerPosition
     mouse_pos = Save.PlayerPosition
