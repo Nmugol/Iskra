edf488e144b3269d4148560895458969fb1a08a0
Norbert <nmugol000@gmail.com>
2025-04-15 12:22:51 +0200

    Dodanie animacji gracza

--- Script/Player/PlayerController.gd
+++ Script/Player/PlayerController.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 @export var speed: float = 100
 @export var gravity: float = 980
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
+@onready var sprite: AnimatedSprite2D = $Sprite2D
 
 func _ready() -> void:
 	Signals.save_game.connect(SavePlayerData)
-	
+	sprite.play("idle")
 	position = Save.PlayerPosition
 
 func _physics_process(_delta: float) -> void:
@@ -18,9 +19,19 @@ func _physics_process(_delta: float) -> void:
 
 	if nav.is_navigation_finished():
 		velocity = Vector2.ZERO
+		sprite.play("idle")
+		
 	else:
 		var direction = (nav.get_next_path_position() - global_position).normalized()
 		velocity = direction * speed
+		
+		if velocity.x > 0:
+			sprite.flip_h = false
+			
+		else:
+			sprite.flip_h = true
+			
+		sprite.play("walk")
 
 	move_and_slide()
 
