ebc44e8049e47142a8c40292110fcbe3f4d6e8b8
Nmugol <nmugol000@gmail.com>
2025-04-17 18:53:54 +0200

    Dodanie podstawowych dekoracji oraz śceiżki po której może się poruszać gracz

--- Script/Class/Exit.gd
+++ Script/Class/Exit.gd
@@ -3,6 +3,8 @@ class_name Exit
 
 @export var location_path: String
 
+@export_category("Arrea")
+@export var area_shape: CollisionShape2D
 @export var min_distance: float = 500
 
 @export_category("Player")
@@ -10,7 +12,8 @@ class_name Exit
 @export var stopping_point: Vector2
 @export var target_player_position : Vector2 = Vector2(0,0)
 
-var dist
+var start_point: Vector2 = Vector2(0,0)
+var dist: int
 var mouse_hover: bool = false
 const  MainScene = "res://Scenes/World.tscn"
 
@@ -19,8 +22,15 @@ var in_scene: bool = true
 func  _ready() -> void:
 	in_scene = true
 	mouse_entered.connect(func():
-		dist = floor(global_transform.origin.distance_to(player.global_transform.origin))
-
+		
+		
+		
+		print("Pozycja obiektu",position)
+		print("pozycja gracza", player.global_position)
+		
+		CalculateDistance()
+		
+		print("Dystans", dist)
 		mouse_hover = true
 		)
 
@@ -28,12 +38,16 @@ func  _ready() -> void:
 		mouse_hover = false
 		)
 
+func CalculateDistance() -> void:
+	var d1 = sqrt(pow(area_shape.global_position.x - start_point.x,2)+pow(area_shape.global_position.y-start_point.y,2))
+	var d2 = sqrt(pow(player.global_position.x - start_point.x,2)+pow(player.global_position.y-start_point.y,2))
+	dist = floor(abs(d2 - d1))
+
 func _process(_delta: float) -> void:
 	if mouse_hover:
 		if Input.is_action_just_pressed("MovePlayer") and dist <= min_distance:
 
 			in_scene = false
-			print(dist)
 
 			Save.CurrentScenePath = location_path
 			Save.PlayerPosition = target_player_position
@@ -43,5 +57,6 @@ func _process(_delta: float) -> void:
 
 
 			player.nav.target_position = stopping_point
+
+			CalculateDistance()
 			print(dist)
-			dist = floor(position.distance_to(player.position))
