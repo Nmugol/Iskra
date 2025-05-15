092d01ba132721f8e629d182fa33d097ca1bce97
Nmugol <nmugol000@gmail.com>
2025-05-15 19:20:25 +0200

    poprawa enumow

--- Script/NPC.gd
+++ Script/NPC.gd
@@ -5,33 +5,37 @@ extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 enum npc_names{
-	EmilSchmidt, 
-	MiriamSchmidt, 
-	James, 
-	Jonas,  
-	Zygmunt, 
-	Peter,
-	Guards1,
-	Guards2,
-	Guards3,
-	Guards4,
-	Guards5,
-	Guards6,
-	Guards7,
-	Guards8,
-	Hania,
-	Kacper,
-	Kamila,
-	Patryk,
-	Przemek
-	}
-@export var npc_name = npc_names.Guards1:
+	EMIL_SCHMIDT,
+	MIRIAM_SCHMIDT,
+	JAMES,
+	JONAS,
+	ZYGMUNT,
+	PETER,
+	GUARD_1,
+	GUARD_2,
+	GUARD_3,
+	GUARD_4,
+	GUARD_5,
+	GUARD_6,
+	GUARD_7,
+	GUARD_8,
+	HANIA,
+	KACPER,
+	KAMILA,
+	PATRYK,
+	PRZEMEK
+}
+@export var npc_name: npc_names = npc_names.GUARD_1:
 	set(value):
 		npc_name = value
 		update_animation()
 	get: return npc_name
 
-@export_enum("Idle", "Walk") var animation_name = "Idle":
+enum  animation_mames{
+	IDLE, WALK
+}
+
+@export var animation_name: animation_mames = animation_mames.IDLE:
 	set(value):
 		animation_name = value
 		update_animation()
@@ -43,27 +47,27 @@ enum npc_names{
 	get: return flip_sprite
 
 func setup(anim: String, flip: bool):
-	animation_name = anim
+	animation_name = animation_mames[anim.to_upper()]
 	flip_sprite = flip
 
 func update_state(anim: String, flip: bool):
-	animation_name = anim
+	animation_name = animation_mames[anim.to_upper()]
 	flip_sprite = flip
 
 func update_animation():
 	if not is_instance_valid(sprite) or not sprite.sprite_frames:
 		return  # Zabezpieczenie przed dostępem do niezainicjalizowanego węzła
 	
-	var ak = npc_names.keys()[npc_names.values().find(npc_name)]
-	
-	var target_animation = "%s_%s" % [ak, animation_name]
+	var ak: String = npc_names.keys()[npc_names.values().find(npc_name)]
+	var ak2: String = animation_mames.keys()[animation_mames.values().find(animation_name)]
+	var target_animation = "%s_%s" % [ak.capitalize().replace(" ","_"), ak2.capitalize()]
 	
 	if sprite.sprite_frames.has_animation(target_animation):
 		
 		match animation_name:
-			"Idle": 
+			animation_mames.IDLE: 
 				sprite.scale = Vector2(0.5, 0.5)
-			"Walk": 
+			animation_mames.WALK: 
 				sprite.scale = Vector2(0.667, 0.667)
 				
 		for i in 2:
