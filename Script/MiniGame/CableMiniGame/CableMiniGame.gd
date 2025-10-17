987d2b60ecd20a2cf1f4d6a2cc24e0be40d472db
Norbert MUzolf <nmugol000@gmail.com>
2025-10-17 16:47:32 +0200

    fixing cable tail and poslis texure

--- Script/MiniGame/CableMiniGame/CableMiniGame.gd
+++ Script/MiniGame/CableMiniGame/CableMiniGame.gd
@@ -1,68 +1,83 @@
 extends Node2D
 
 const LEVEL_DATA = [
-
+	#Level1
 	{
 		"size": Vector2i(5, 5),
-		"source": [Vector2i(0, 2)], 
-		"target": [Vector2i(4, 2)],
+		"source": [Vector2i(0, 1)], 
+		"target": [Vector2i(4, 4)],
 		"tiles": [
-			[0, 2, 0, 0], 
-			[1, 2, 0, 0], 
-			[2, 2, 0, 0], 
-			[3, 2, 0, 0], 
-			[4, 2, 0, 0],
+			[0,0,4,1],[1,0,4,3],[2,0,0,2],[3,0,0,1],[4,0,1,1],
 
-			[0, 0, 1, 3], 
-			[1, 0, 4, 0], 
-			[2, 0, 2, 1], 
-			[3, 0, 5, 2], 
-			[4, 0, 1, 0],
+			[0,1,3,1],[1,1,4,2],[2,1,5,3],[3,1,0,2],[4,1,4,2],
+			
+			[0,2,0,1],[1,2,0,1],[2,2,0,0],[3,2,2,3],[4,2,1,2],
 
-			[0, 1, 0, 0], 
-			[1, 1, 3, 0], 
-			[2, 1, 0, 1], 
-			[3, 1, 4, 3], 
-			[4, 1, 2, 2], 
+			[0,3,4,0],[1,3,2,0],[2,3,0,0],[3,3,4,1],[4,3,2,3],
 
-			[0, 3, 2, 1], 
-			[1, 3, 4, 1], 
-			[2, 3, 5, 0], 
-			[3, 3, 1, 2], 
-			[4, 3, 0, 0], 
+			[0,4,5,0],[1,4,0,2],[2,4,5,0],[3,4,4,1],[4,4,3,3],
+		]
+	},
+	#Level2
+	{
+		"size": Vector2i(5, 5),
+		"source": [Vector2i(0, 0),Vector2i(0,4)], 
+		"target": [Vector2i(3, 1),Vector2i(4,3)],
+		"tiles": [
+			[0,0,0,0],[1,0,2,1],[2,0,0,0],[3,0,0,0],[4,0,1,1],
 
-			[0, 4, 0, 1], 
-			[1, 4, 2, 0], 
-			[2, 4, 3, 0], 
-			[3, 4, 1, 3], 
-			[4, 4, 5, 1], 
+			[0,1,0,1],[1,1,0,2],[2,1,5,2],[3,1,0,1],[4,1,1,2],
+
+			[0,2,2,2],[1,2,5,2],[2,2,4,1],[3,2,0,2],[4,2,1,3],
+
+			[0,3,3,3],[1,3,0,0],[2,3,0,3],[3,3,1,2],[4,3,0,0],
+
+			[0,4,0,1],[1,4,1,0],[2,4,2,0],[3,4,0,1],[4,4,1,2],
+		]
+	},
+	#Level3
+	{
+		"size": Vector2i(5, 5),
+		"source": [Vector2i(1, 1)], 
+		"target": [Vector2i(0, 4),Vector2i(3,3), Vector2i(4,0)],
+		"tiles": [
+			[0,0,3,1],[1,0,1,3],[2,0,0,1],[3,0,1,0],[4,0,0,0],
+
+			[0,1,2,0],[1,1,1,1],[2,1,0,0],[3,1,5,1],[4,1,4,1],
+
+			[0,2,2,3],[1,2,1,0],[2,2,0,0],[3,2,3,3],[4,2,0,0],
+
+			[0,3,0,0],[1,3,2,3],[2,3,1,0],[3,3,0,1],[4,3,1,1],
+
+			[0,4,0,1],[1,4,0,1],[2,4,1,1],[3,4,5,1],[4,4,0,0],
 		]
 	},
 ]
 
 @export_category("Grid Settings")
 @export var grid_size: Vector2i = Vector2i(5,5)
-@export var tile_scene: PackedScene # Wymaga CableTile.tscn
+@export var tile_scene: PackedScene 
 @export var tile_size: float = 32.0
 @export var tile_container: Node2D
 
 @export_category("Energi input and output")
-@export var source_positions: = [Vector2i(0, 2)] 
-@export var target_positions: = [Vector2i(4, 2)] 
+@export var source_positions: Array
+@export var target_positions: Array
 
-var grid: Array[Array] = [] # Tablica przechowująca instancje CableTile
+var grid: Array[Array] = []
 var is_game_active: bool = false
-
 var current_level_index: int = 0
 
 func _ready():
-
-	# KLUCZOWA POPRAWKA: Łączenie się z globalnym sygnałem
+	if not is_instance_valid(tile_container):
+		tile_container = Node2D.new()
+		tile_container.name = "TileContainer"
+		add_child(tile_container)
+		
 	Signals.tile_rotated.connect(_on_tile_rotated)
-
 	load_level(current_level_index)
 
-func initialize_grid(tile_configs: Array): # Teraz przyjmuje argument
+func initialize_grid(tile_configs: Array):
 	if not tile_scene:
 		push_error("Błąd: Tile Scene nie jest przypisana!")
 		return
@@ -71,8 +86,7 @@ func initialize_grid(tile_configs: Array): # Teraz przyjmuje argument
 	for x in grid_size.x:
 		grid[x] = []
 		grid[x].resize(grid_size.y)
-		
-	# Tworzenie kafelków na podstawie konfiguracji
+
 	for config in tile_configs:
 		var x = config[0]
 		var y = config[1]
@@ -85,15 +99,18 @@ func initialize_grid(tile_configs: Array): # Teraz przyjmuje argument
 		tile_instance.position = Vector2(x * tile_size, y * tile_size)
 		tile_instance.grid_position = Vector2i(x, y)
 		
-		# Ustawienie stanu kafelka na podstawie danych poziomu
 		tile_instance.cable_type = type
 		tile_instance.rotation_step = _rotation
-		tile_instance.sprite.rotation_degrees = _rotation * 90 # Wizualna aktualizacja
+		tile_instance.sprite.rotation_degrees = _rotation * 90 
+		
+		if source_positions.has(tile_instance.grid_position) or target_positions.has(tile_instance.grid_position):
+			tile_instance.is_target_or_source = true
+		
+		tile_instance.set_bg()
 		
 		grid[x][y] = tile_instance
 
 func load_level(index: int):
-	# Sprawdzenie zakresu poziomu
 	if index < 0 or index >= LEVEL_DATA.size():
 		print("Błąd: Próba załadowania nieistniejącego poziomu!")
 		return
@@ -101,100 +118,107 @@ func load_level(index: int):
 	current_level_index = index
 	var level_data = LEVEL_DATA[index]
 
-	# Ustawianie rozmiaru
 	grid_size = level_data.size
 	
-	# FIX: Jawne rzutowanie tablic na oczekiwany typ Array[Vector2i].
-	# To rozwiązuje błąd typowania (Trying to assign an array of type "Array" 
-	# to a variable of type "Array[Vector2i]").
-	source_positions = level_data.source as Array[Vector2i]
-	target_positions = level_data.target as Array[Vector2i]
+	source_positions = level_data.source
+	target_positions = level_data.target
 
-	# 1. Resetowanie i usuwanie starej planszy
 	if tile_container:
 		for child in tile_container.get_children():
 			child.queue_free()
 	grid.clear()
 	
-	# 2. Inicjalizacja nowej siatki z danymi poziomu
-	# Wymaga, aby funkcja initialize_grid(tile_configs: Array) istniała.
 	initialize_grid(level_data.tiles)
-
-	# 3. Uruchomienie logiki gry
 	start_game()
 
 func start_game():
 	is_game_active = true
-	# Nie robimy już losowego generowania. Logika pozostaje czysta:
 	recalculate_power_flow()
 
-# POPRAWKA: Dodano podkreślenie, aby zignorować nieużywany parametr (GRID_POS)
 func _on_tile_rotated(_grid_pos: Vector2i):
 	if is_game_active:
 		recalculate_power_flow()
 
 func get_tile(pos: Vector2i) -> CableTile:
-	if pos.x < 0 or pos.x >= grid_size.x or pos.y < 0 or pos.y >= grid_size.y:
-		return null
+	if pos.x < 0 or pos.x >= grid_size.x or pos.y < 0 or pos.y >= grid_size.y: return null
 	return grid[pos.x][pos.y]
 
 func recalculate_power_flow():
-	# 1. Reset stanu zasilania
 	var tiles_to_check: Array[Vector2i] = []
+	var powered_tiles_info: Dictionary = {} 
 	
+	# 1. Resetowanie wszystkich kafelków
 	for x in grid_size.x:
 		for y in grid_size.y:
 			var tile: CableTile = grid[x][y]
 			tile.set_power_state(false, [])
-			
-	# Inicjalizuj BFS od źródeł
+
+	# 2. Dodanie źródeł do kolejki
 	for source_pos in source_positions:
 		var source_tile: CableTile = get_tile(source_pos)
 		if source_tile:
-			tiles_to_check.append(source_pos)
+			if not powered_tiles_info.has(source_pos):
+				powered_tiles_info[source_pos] = [] as Array[Vector2i]
+		
+			if not powered_tiles_info[source_pos].has(Vector2i.ZERO):
+				powered_tiles_info[source_pos].append(Vector2i.ZERO)
+				tiles_to_check.push_back(source_pos)
+			
 
-	var powered_tiles_set: Dictionary = {}
-
-	# 2. Algorytm Przeszukiwania Wszerz (BFS)
 	while !tiles_to_check.is_empty():
 		var current_pos: Vector2i = tiles_to_check.pop_front()
 		var current_tile: CableTile = get_tile(current_pos)
 		
-		if powered_tiles_set.has(current_pos):
-			continue
+		if current_tile == null: continue
+		var incoming_dirs: Array[Vector2i] = powered_tiles_info.get(current_pos, [] as Array[Vector2i]) as Array[Vector2i]
+		
+		current_tile.set_power_state(true, incoming_dirs)
+		current_tile.set_bg()
 
-		powered_tiles_set[current_pos] = true
-		current_tile.set_power_state(true, [])
+		var all_output_directions: Array[Vector2i] = []
+		for power_entered_from in incoming_dirs:
+			var output_connections = current_tile.get_output_dir(power_entered_from)
+			for dir in output_connections:
+				if not all_output_directions.has(dir):
+					all_output_directions.append(dir)
 
-		var connections = current_tile.get_connections()
-
-		for connection_dir in connections:
+		for connection_dir in all_output_directions:
 			var neighbor_pos = current_pos + connection_dir
 			var neighbor_tile: CableTile = get_tile(neighbor_pos)
-
-			if neighbor_tile and not powered_tiles_set.has(neighbor_pos):
-				# KLUCZOWA WERYFIKACJA: Sprawdź, czy sąsiad ma port wejściowy
-				var incoming_dir = -connection_dir 
+			
+			if neighbor_tile == null: continue
+			var incoming_dir_to_neighbor = -connection_dir 
+			var neighbor_all_open_ports = neighbor_tile.get_output_dir(Vector2i.ZERO)
+			if neighbor_all_open_ports.has(incoming_dir_to_neighbor):
+				var is_new_path = false
 				
-				if neighbor_tile.get_connections().has(incoming_dir):
+				if not powered_tiles_info.has(neighbor_pos):
+					powered_tiles_info[neighbor_pos] = [] as Array[Vector2i]
+					is_new_path = true
+				
+				if not powered_tiles_info[neighbor_pos].has(incoming_dir_to_neighbor):
+					powered_tiles_info[neighbor_pos].append(incoming_dir_to_neighbor)
+					is_new_path = true
+					
+				if is_new_path:
 					tiles_to_check.push_back(neighbor_pos)
-	
-	# 3. Sprawdzenie warunku zwycięstwa
-	check_win_condition(powered_tiles_set)
+	check_win_condition(powered_tiles_info.keys())
 
-func check_win_condition(powered_tiles_set: Dictionary):
-	if not is_game_active:
-		return
+func check_win_condition(powered_tiles: Array):
+	if not is_game_active: return
 		
 	var all_targets_powered = true
 	for target_pos in target_positions:
-		if not powered_tiles_set.has(target_pos):
+		if not powered_tiles.has(target_pos):
 			all_targets_powered = false
 			break
-	
-	if all_targets_powered:
-		game_won()
+	if all_targets_powered: game_won()
 
 func game_won():
 	is_game_active = false
 	print("WYGRANA! Wszystkie wyjścia są zasilone!")
+	
+	var next_level_index = current_level_index + 1
+	
+	if next_level_index < LEVEL_DATA.size(): load_level(next_level_index)
+	else: print("GRATULACJE! Ukończono wszystkie poziomy!")
--- Script/MiniGame/CableMiniGame/CableTile.gd
+++ Script/MiniGame/CableMiniGame/CableTile.gd
@@ -1,39 +1,40 @@
 extends Node2D
 class_name CableTile
 
-# ZMIENNE TYPÓW ENUM (niezmienione)
 enum CableType {
-	STRAIGHT,     # Prosty
-	CORNER_L,     # Zakręt w lewo
-	CORNER_R,     # Zakręt w prawo
-	CROSS,        # Skrzyżowanie
-	T_JUNCTION,   # Skrzyżowanie w kształcie litery T
-	DUAL_CORNER   # Jednoczesny skręt L/P (z dołu w lewo, z góry w prawo)
+	STRAIGHT,     # 0
+	CORNER_L,     # 1
+	CORNER_R,     # 2
+	CROSS,        # 3
+	T_JUNCTION,   # 4
+	DUAL_CORNER   # 5
 }
 
-# STAŁE KIERUNKÓW (niezmienione)
-const UP = Vector2i(0, -1)
+const UP = Vector2i(0, -1) 
 const DOWN = Vector2i(0, 1)
 const LEFT = Vector2i(-1, 0)
 const RIGHT = Vector2i(1, 0)
 
-# MAPA POŁĄCZEŃ (niezmieniona)
 const CONNECTION_MAP = {
 	CableType.STRAIGHT: [[UP, DOWN], [LEFT, RIGHT], [DOWN, UP], [RIGHT, LEFT]],
 	CableType.CORNER_L: [[DOWN, LEFT], [LEFT, UP], [UP, RIGHT], [RIGHT, DOWN]],
-	CableType.CORNER_R: [[DOWN, RIGHT], [RIGHT, UP], [UP, LEFT], [LEFT, DOWN]],
+	CableType.CORNER_R: [[DOWN, RIGHT], [DOWN, LEFT], [UP, LEFT], [UP, RIGHT]],
 	CableType.CROSS: [[UP, DOWN, LEFT, RIGHT], [UP, DOWN, LEFT, RIGHT], [UP, DOWN, LEFT, RIGHT], [UP, DOWN, LEFT, RIGHT]],
-	CableType.T_JUNCTION: [[DOWN, LEFT, RIGHT], [UP, DOWN, LEFT], [UP, LEFT, RIGHT], [UP, DOWN, RIGHT]],
-	CableType.DUAL_CORNER: [[DOWN, LEFT, UP, RIGHT], [LEFT, UP, RIGHT, DOWN], [UP, RIGHT, DOWN, LEFT], [RIGHT, DOWN, LEFT, UP]],
+	CableType.T_JUNCTION: [[DOWN, LEFT, RIGHT], [UP, DOWN, LEFT], [UP, LEFT, RIGHT], [UP, DOWN, RIGHT]], 
 }
 
-# WŁAŚCIWOŚCI
+const DUAL_CORNER_LOGIC = {
+	0: {UP: [RIGHT], RIGHT: [UP], DOWN: [LEFT], LEFT: [DOWN]},
+	1: {RIGHT: [DOWN], DOWN: [RIGHT], UP: [LEFT], LEFT: [UP]},
+	2: { DOWN: [LEFT], UP: [RIGHT], LEFT: [DOWN], RIGHT: [UP]},
+	3: {LEFT: [UP], UP: [LEFT], RIGHT: [DOWN], DOWN: [RIGHT]}
+}
+
 @export_category("Type")
 @export var cable_type: CableType = CableType.STRAIGHT:
 	set(value):
 		cable_type = value
 		if sprite:
-			# POPRAWKA: Jawne rzutowanie na int, aby uniknąć ostrzeżenia INT_AS_ENUM_WITHOUT_CAST
 			sprite.play(CableType.keys()[int(cable_type)])
 	get():
 		return cable_type
@@ -41,44 +42,62 @@ const CONNECTION_MAP = {
 @export_category("Sprite")
 @export var sprite: AnimatedSprite2D
 @export_range(0,3,1) var rotation_step: int = 0
+@export var bg: AnimatedSprite2D
 
 @export_category("Position")
 @export var grid_position: Vector2i = Vector2i.ZERO
 
 var neighbors_powered_from: Array[Vector2i] = []
 var is_powered: bool = false
-var game_is_start: bool = false # Stan blokujący interakcję, ustawiany przez GameManager
+var game_is_start: bool = false
 
-# USUNIĘTO: Funkcja _process i zmienna mouse_on (nadmiarowa logika kliknięcia)
-# func _process(_delta: float) -> void: pass
+var is_target_or_source: bool = false
+
+func _ready() -> void:
+	# Ustawia stan początkowy na "wyłączony"
+	set_power_state(false, [])
+
+func set_bg()->void:
+	if is_target_or_source:
+		bg.show()
+		if is_powered:
+			bg.play("Active")
+		else:
+			bg.play("Deactivated")
+	else:
+		bg.hide()
 
 func rotation_tile()->void:
 	rotation_step = (rotation_step + 1) % 4
 	sprite.rotation_degrees = rotation_step * 90
-
-	# Emitowanie globalnego sygnału z poprawnym argumentem (grid_position)
 	Signals.tile_rotated.emit(grid_position)
 
-# FUNKCJA UŻYWANA PRZEZ BFS W GAME MANAGERZE
-func get_connections():
-	if rotation_step < 0 or rotation_step >= 4: return[]
-	var type_map = CONNECTION_MAP.get(cable_type)
-	if type_map == null: return []
-	return type_map[rotation_step]
 
-# FUNKCJA AKTUALIZUJĄCA STAN ZASILANIA
+func get_output_dir(incoming_dir: Vector2i = Vector2i.ZERO):
+	if cable_type == CableType.DUAL_CORNER:
+		var paths = DUAL_CORNER_LOGIC.get(rotation_step, {})
+		if incoming_dir == Vector2i.ZERO:
+			return [UP, DOWN, LEFT, RIGHT] as Array[Vector2i]
+		return paths.get(incoming_dir, []) as Array[Vector2i]
+	if rotation_step < 0 or rotation_step >= 4: 
+		return [] as Array[Vector2i]
+	
+	var type_map = CONNECTION_MAP.get(cable_type)
+	if type_map == null: return [] as Array[Vector2i]
+		
+	var all_open_ports = type_map[rotation_step]
+	if incoming_dir != Vector2i.ZERO and all_open_ports.has(incoming_dir):
+		var output = all_open_ports.duplicate()
+		output.erase(incoming_dir)
+		return output
+			
+	return all_open_ports
+
 func set_power_state(powered: bool, powered_from: Array[Vector2i]) -> void:
 	is_powered = powered
 	neighbors_powered_from = powered_from
 
-	# Aktualizacja wizualna
-	if is_powered:
-		sprite.modulate = Color.YELLOW
-	else:
-		sprite.modulate = Color.WHITE
-
-# OBSŁUGA KLIKNIĘCIA (połączona ze sygnałem input_event z Area2D)
 func _on_area_2d_input_event(_viewport:Node, event:InputEvent, _shape_idx:int) -> void:
-	if game_is_start: return # Zablokowanie interakcji
+	if game_is_start or is_target_or_source: return 
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		rotation_tile()
